dbd92e34ab6bf2a580429462febc38d46feda991
Eivind Naasen <einaa7293@uib.no>
2026-04-22 11:53:21 +0200

    Base class of simulations

---
+++ Simulation.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+class Simulation {
+public:
+    virtual const Rect& getBounds() const = 0;
+    virtual std::vector<Ball>& getBalls() = 0;
+    virtual const Grid<std::vector<Ball*>>& getGrid() const = 0;
+    virtual ~Simulation() = default;
+};
